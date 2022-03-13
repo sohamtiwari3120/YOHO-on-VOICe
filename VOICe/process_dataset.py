ad89ba588815170e839e9aabb04df5d42ccc5621
Soham Tiwari <soham.tiwari800@gmail.com>
2022-03-13 15:03:58 +0530

    feat: added cli args for process dataset

--- VOICe/process_dataset.py
+++ VOICe/process_dataset.py
@@ -1,16 +1,19 @@
+import argparse
+from ast import arg
 from loguru import logger
 from config import snr
 from utils.data_utils import generate_windows_and_anns, save_logmelspec_and_labels, convert_to_mono, envs, data_modes
 logger.add('process_dataset.log', rotation='500 KB')
 
 @logger.catch
-def main():
+def main(args):
     logger.info(f'Starting processing of entire dataset for {snr} audio.')
     logger.info(f'First converting all audios to mono.')
-    convert_to_mono()
+    if args.convert_to_mono:
+        convert_to_mono()
     logger.info(f'Finished conversion to mono audio.')
-    for mode in data_modes:
-        for env in envs:
+    for mode in args.data_modes:
+        for env in args.envs:
             logger.info(f'Processing {mode}, {env} data.')
             audio_windows, labels = generate_windows_and_anns(mode, env)
             logmel_path, label_path = save_logmelspec_and_labels(mode, env, audio_windows, labels)
@@ -20,4 +23,9 @@ def main():
     logger.info(f'Finished processing entire dataset.')
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Program to process dataset by converting to mono audios and then generating and saving spectrograms.')
+    parser.add_argument('-ctm', '--convert_to_mono', type=bool, default=True)
+    parser.add_argument('-dms', '--data_modes', nargs='+', default=data_modes)
+    parser.add_argument('-es', '--envs', nargs='+', default=envs)
+    args = parser.parse_args()
+    main(args)
