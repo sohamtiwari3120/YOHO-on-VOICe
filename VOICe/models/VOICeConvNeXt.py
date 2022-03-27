2c52e474e35fcb23b89cf504c36a91f86729f8e0
Soham Tiwari <soham.tiwari800@gmail.com>
2022-03-27 11:02:58 +0530

    fix: Using convnext pretrained false

--- VOICe/models/VOICeConvNeXt.py
+++ VOICe/models/VOICeConvNeXt.py
@@ -18,7 +18,7 @@ class VOICeConvNeXt(nn.Module):
         self.input_height = input_height
         self.input_width = input_width
         self.convert_channels_to_3 = nn.Conv2d(1, 3, (1, 1))
-        self.convnext = convnext_tiny(True)
+        self.convnext = convnext_tiny(False)
         # output shape
         self.head = nn.Sequential(
             nn.Linear(1000, 512),
@@ -29,7 +29,7 @@ class VOICeConvNeXt(nn.Module):
         )
 
     def forward(self, input):
-        x = self.convert_channels_to_3(input)
+        x = self.convert_channels_to_3(input.float())
         x = self.convnext(x)
         x = self.head(x) # -> (batch_size, 3*num_classes)
         x = torch.unsqueeze(x, dim=-2) # -> (batch_size, 1(=num_subwindows), 3*num_classes)
