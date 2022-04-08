35bf3f42cf3ecea30b083146655dea94cb1a7d01
Soham Tiwari [paperspace-ntu] <soham.tiwari800@gmail.com>
2022-04-09 00:49:29 +0530

    fix: corrected test coatnet and pann

--- VOICe/test_coatnet.py
+++ VOICe/test_coatnet.py
@@ -1,9 +1,9 @@
 from audioop import add
 from config import add_EAP_to_path
 add_EAP_to_path()
-import model.attention.CoAtNet as CN
+from model.attention.CoAtNet import CoAtNet as CN
 def main():
-    model = CN()
+    model = CN(3, 257)
     print(model)
 
 if __name__ == '__main__':
--- VOICe/test_pann.py
+++ VOICe/test_pann.py
@@ -1,14 +1,15 @@
 import torch
 from torchsummary import summary
 from config import hparams
-from models.pann_encoder import Cnn10
+from models.pann_encoder import Cnn14, VOICePANN
 
 hp = hparams()
 
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = Cnn10()
+    model = Cnn14()
     summary(model.to(device), (hp.input_height, 64))
+    print(VOICePANN())
 
 if __name__ == '__main__':
     main()
