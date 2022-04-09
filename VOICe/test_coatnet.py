dc41bd03296802bd588f5f54a663283102ce9ab0
Soham Tiwari <soham.tiwari800@gmail.com>
2022-04-09 09:34:49 +0530

    fix: added model summary to test coatnet

--- VOICe/test_coatnet.py
+++ VOICe/test_coatnet.py
@@ -1,10 +1,14 @@
-from audioop import add
+import torch
 from config import add_EAP_to_path
+from torchsummary import summary
 add_EAP_to_path()
 from model.attention.CoAtNet import CoAtNet as CN
 def main():
+
     model = CN(3, 257)
     print(model)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    summary(model.to(device), (3, 257, 40))
 
 if __name__ == '__main__':
     main()
