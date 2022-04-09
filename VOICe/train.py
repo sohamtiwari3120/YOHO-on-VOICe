1dd620bdb146fb0fba5a7892bf1e5386c796aaee
Soham Tiwari <soham.tiwari800@gmail.com>
2022-04-09 23:23:07 +0530

    fix: added voicecoatnet import to train py file

--- VOICe/train.py
+++ VOICe/train.py
@@ -4,6 +4,7 @@ from models.YOHO_tf import YohoTF
 from models.VOICeConvNeXt import VOICeConvNeXt
 from models.pann_encoder import VOICePANN
 from models.ViT import VOICeViT
+from models.VOICeCoAtNet import VOICeCoAtNet
 from utils.data_utils import VOICeDataModule
 from pytorch_lightning import Trainer
 from pytorch_lightning import loggers as pl_loggers
