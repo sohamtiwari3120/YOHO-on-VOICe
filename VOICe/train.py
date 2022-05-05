bc0fa294027ca0af8ef216a6842a76064ffe5643
Soham Tiwari <soham.tiwari800@gmail.com>
2022-05-06 00:22:48 +0530

    fix: indented wandb logger watch model

--- VOICe/train.py
+++ VOICe/train.py
@@ -81,7 +81,7 @@ def pytorch(args):
         wandb_logger.experiment.config.update(
             hp.__dict__, allow_val_change=True)
         wandb_logger.experiment.config.update(args, allow_val_change=True)
-    wandb_logger.watch(model)
+        wandb_logger.watch(model)
 
     if args.model_summary:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
